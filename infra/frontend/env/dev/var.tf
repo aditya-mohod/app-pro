76deee7eeebbb0d88c50bc5abe039ffc3a6a1b72
aditya-mohod <mohodaditya123@gmail.com>
2026-09-15 17:23:15 +0530

    Update var.tf

--- infra/frontend/env/dev/var.tf
+++ infra/frontend/env/dev/var.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "domain_name" {
   description = "Domain name to use as the S3 bucket name"
   type        = string
-  default = "deployway.online"
+  default = "terraform-state-bucket-cbz-kharadi-09"
 }
 
 
